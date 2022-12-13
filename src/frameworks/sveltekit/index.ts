ca607ec55d31627eb3a397684a34d8643e447f9f
Austin Crim <aust.crim@gmail.com>
2022-12-13 16:42:40 -0600

    static sk build working?

--- src/frameworks/sveltekit/index.ts
+++ src/frameworks/sveltekit/index.ts
@@ -1,16 +1,26 @@
-import { FrameworkType } from "..";
+import { copy } from "fs-extra";
+import { join } from "path";
+import { FrameworkType, SupportLevel } from "..";
 import { viteDiscoverWithNpmDependency } from "../vite";
-
-export * from "../vite";
+// TODO figure out why relativeRequire was not working
+const { dynamicImport } = require(true && "../../dynamicImport");
 
 export const name = "SvelteKit";
+export const support = SupportLevel.Experimental;
 export const type = FrameworkType.MetaFramework;
+export { build } from "../vite";
 
 // export const init = initViteTemplate("svelte");
 export const discover = viteDiscoverWithNpmDependency("@sveltejs/kit");
 
 export async function ɵcodegenPublicDirectory(root: string, dest: string) {
-  console.log("codegenPublicDirectory sveltekit");
+  const config = await dynamicImport(join(root, "svelte.config.js"));
+  const outDir = config.outDir || ".svelte-kit";
+  const prerenderedPath = join(root, outDir, "output", "prerendered", "pages");
+  const assetsPath = join(root, outDir, "output", "client");
+
+  console.log("copying files...");
+  await Promise.all([copy(prerenderedPath, dest), copy(assetsPath, dest)]);
 }
 
 export async function ɵcodegenFunctionsDirectory(sourceDir: string, destDir: string) {
